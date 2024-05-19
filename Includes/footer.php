91b840122ec255beae6b0215fee1f32a1d3e9663
Abdeljalil Aziz <125446723+jalilaziz@users.noreply.github.com>
2024-05-20 00:55:26 +0100

    Add files via upload

---
+++ Includes/footer.php
@@ -0,0 +1,27 @@
+		
+		<footer class="footer_section">
+			<div class="container">
+						<div class="copyright"  >
+							Barber Top © 
+							<script type="text/javascript"> 
+								document.write(new Date().getFullYear())
+							</script>
+						</div>
+			</div>
+		</footer>
+
+	
+		<!-- INCLUDE JS SCRIPTS -->
+
+		<script src="Design/js/jquery.min.js"></script>
+		<script src="Design/js/bootstrap.min.js"></script>
+		<script src="Design/js/bootstrap.bundle.min.js"></script>
+		<script src="Design/js/main.js"></script>
+
+	</body>
+
+	<!-- END BODY TAG -->
+
+</html>
+
+<!-- END HTML TAG -->
